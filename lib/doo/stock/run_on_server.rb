b15011bc25648f29036eb26e7119c292a473b57c
Mat Trudel <mat@geeky.net>
2010-09-16 17:09:34 -0400

    Added gateway

--- lib/doo/stock/run_on_server.rb
+++ lib/doo/stock/run_on_server.rb
@@ -1,25 +1,35 @@
-SshCommand = "ssh -S '~/.ssh/master-%l-%r@%h:%p'"
 Doo::Base.class_eval do
   def run_on_server(servers, variables = {}, &block)
     servers.each do |host, params|
       with_clone(variables.merge({:host => host}).merge(params || {})) do
         def run(cmd, opt = {})
-          puts "Running ssh #{(!opt.include? :pty || opt[:pty])? '-t' : ''} #{host} \"#{cmd}\"" if verbose
-          system("#{SshCommand} #{(!opt.include? :pty || opt[:pty])? '-t' : ''} #{host} \"#{cmd}\"") || raise("SSH Error") unless dry_run
+          cmdopts = ["-S \"~/.ssh/master-%l-%r@%h:%p\""]
+          cmdopts << "-t" if opt.include? :pty || opt[:pty]
+          command = "ssh #{cmdopts.join(' ')} #{user}@#{host} #{cmd}"
+          puts "Running #{command}" if verbose
+          system(command) || raise("SSH Error") unless dry_run
           $?
         end
-      
+
         def put(local, remote)
           puts "scp -r \"#{local}\" \"#{host}:#{remote}\"" if verbose
           system("scp -r \"#{local}\" \"#{host}:#{remote}\"") || raise("SSH Error") unless dry_run
           $?
         end
-      
+
         begin
-          system("#{SshCommand} -MNf #{host}") || raise("SSH Error") unless dry_run
+          cmdopts = ["-MNf -S \"~/.ssh/master-%l-%r@%h:%p\""]
+          cmdopts << "-oProxyCommand=\"ssh #{gateway} exec nc %h %p\"" if defined? :gateway
+          command = "ssh #{cmdopts.join(' ')} #{user}@#{host}"
+          puts "Running #{command}" if verbose
+          system(command) || raise("SSH Error") unless dry_run
+          
           instance_eval &block if block_given?
         ensure
-          system("#{SshCommand} -Oexit #{host}") || raise("SSH Error") unless dry_run
+          cmdopts = ["-Oexit -S \"~/.ssh/master-%l-%r@%h:%p\""]
+          command = "ssh #{cmdopts.join(' ')} #{user}@#{host}"
+          puts "Running #{command}" if verbose
+          system(command) || raise("SSH Error") unless dry_run
         end
       end
     end
@@ -30,7 +40,7 @@ Hash.class_eval do
   def with_role(role)
     reject { |k,v| ! [v[:role]].flatten.member? role }
   end
-  
+
   def except_role(role)
     reject { |k,v| [v[:role]].flatten.member? role }
   end
