e146b76353c797779c4da5b7c358262222da6de6
Mat Trudel <mat@geeky.net>
2010-12-15 17:35:05 -0500

    Fix to accmmodate bash's backwards notion of truth

--- lib/doo/stock/prereqs.rb
+++ lib/doo/stock/prereqs.rb
@@ -1,9 +1,9 @@
 Doo::Base.class_eval do
   def if_passes(test, &block)
-    yield if run "#{test}"
+    yield unless run "#{test}", :just_return_failure => true
   end
   
   def if_fails(test, &block)
-    yield unless run "#{test}"
-  end
+    yield if run "#{test}", :just_return_failure => true
+  end  
 end
