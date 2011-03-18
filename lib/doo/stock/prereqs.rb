6a0ca165c5a53827c4656f66c85dbb719e82e699
Mat Trudel <mat@geeky.net>
2011-03-18 11:18:22 -0400

    Better equality test

--- lib/doo/stock/prereqs.rb
+++ lib/doo/stock/prereqs.rb
@@ -1,9 +1,9 @@
 Doo::Base.class_eval do
   def if_passes(test, &block)
-    yield unless run "#{test}", :return_on_failure => true
+    yield unless run("#{test}", :return_on_failure => true) != 0
   end
   
   def if_fails(test, &block)
-    yield if run "#{test}", :return_on_failure => true
+    yield if run("#{test}", :return_on_failure => true) != 0
   end  
 end
